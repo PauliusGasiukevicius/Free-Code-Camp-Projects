e63b30103c9f3d8aa45fa35faf9547aa645ff289
Paulius <paulius.gasiukevicius@gmail.com>
2018-08-23 08:01:42 +0300

    Fixed test suite not loading properly bug

--- Free-Code-Camp-Random-Quote-Machine/code.js
+++ Free-Code-Camp-Random-Quote-Machine/code.js
@@ -1,34 +1,44 @@
-let quotes = [];
-quotes.push({
+let quotes = [
+{
    text: "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.",
-   author: "Helen Keller"});
-quotes.push({
+   author: "Helen Keller"
+},
+{
    text: "It is during our darkest moments that we must focus to see the light.",
-   author: "Aristotle"});
-quotes.push({
+   author: "Aristotle"
+},
+{
    text: "Find a place inside where there's joy, and the joy will burn out the pain.",
-   author: "Joseph Campbell"});
-quotes.push({
+   author: "Joseph Campbell"
+},
+{
    text: "Grudges seldom hurt anyone except the one bearing them.",
-   author: "Sherrilyn Kenyon"});
-quotes.push({
+   author: "Sherrilyn Kenyon"
+},
+{
    text: "Nothing is impossible, the word itself says 'I'm possible'!",
-   author: "Audrey Hepburn"});
-quotes.push({
+   author: "Audrey Hepburn"
+},
+{
    text: "Don't judge each day by the harvest you reap but by the seeds that you plant.",
-   author: "Robert Louis Stevenson"});
-quotes.push({
+   author: "Robert Louis Stevenson"
+},
+{
    text: "Believe you can and you're halfway there.",
-   author: "Theodore Roosevelt"});
- quotes.push({
+   author: "Theodore Roosevelt"
+},
+{
    text: "The only true wisdom is in knowing you know nothing.",
-   author: "Socrates"});
- quotes.push({
+   author: "Socrates"
+},
+{
    text: "Life isn't about finding yourself. Life is about creating yourself.",
-   author: "George Bernard Shaw"});
-quotes.push({
+   author: "George Bernard Shaw"
+},
+{
    text: "Problems are not stop signs, they are guidelines.",
-   author: "Robert H. Schuller"});
+   author: "Robert H. Schuller"
+}];
 
 
 let newQuote = function()
